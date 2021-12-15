8a966038d1832103beb0068d380a4cbdaef94044
Aileck <61742408+Aileck@users.noreply.github.com>
2021-12-15 23:47:33 +0100

    Finalize map

---
+++ DANGER/Assets/Question.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Question : MonoBehaviour
+{
+    public bool answered = false;
+    public string quistion;
+    public string optionA;
+    public string optionB;
+    public string optionC;
+
+    public QuizExtraInfo quizExtraInfo;
+
+    public int correcto;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(1) && !answered)
+        {
+            GameObject quizUI = GameObject.FindGameObjectWithTag("QuizUI");
+
+            quizController quiz = quizUI.GetComponent<quizController>();
+            quiz.setCanvasActive(true);
+
+            quiz.SetQuiz(quistion, optionA, optionB, optionC, correcto, quizExtraInfo);
+            answered = true;
+
+        }
+    }
+    
+}
